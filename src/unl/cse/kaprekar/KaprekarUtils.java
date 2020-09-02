adeb164ee86f3c5b2bc341b2a98e35deaaa1aa04
dmryc <dmryc@DESKTOP-I62EPH9.unl.edu>
2020-09-02 08:54:12 -0500

    int * int was giving int, losing needed precision

--- src/unl/cse/kaprekar/KaprekarUtils.java
+++ src/unl/cse/kaprekar/KaprekarUtils.java
@@ -29,7 +29,7 @@ public class KaprekarUtils {
 		} 
 		
 		//we use a long to accommodate larger squares
-		long square = n * n;
+		long square = (long)n * n;
 		int numDigits = (int) Math.log10(n) + 1;
 		long modulus = 1;
 		long first, second;
